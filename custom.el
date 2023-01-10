c87608b27eeaf0cbd4de5e2de66f47dc9c2558a8
aigencharge <aigen.charge@gmail.com>
2023-01-10 18:37:44 -0300

    new file:   custom.el

---
+++ custom.el
@@ -0,0 +1,43 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages '(use-package))
+ '(scroll-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "JetBrains Mono" :foundry "JB" :slant normal :weight normal :height 151 :width normal)))))
+
+;; Melhorar a interface
+
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+; (menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+
+;; Define e inicializa os repositórios dos pacotes
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; Gerenciar os pacotes de maneira mais simples
+;; use-package to simplify the config file
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure 't)
+
+
+
