5aa4a386d01b05c5ab15f1aa50a2879ae31fd5d6
aigencharge <aigen.charge@gmail.com>
2023-01-11 01:54:41 -0300

    add lsp configs

--- custom.el
+++ custom.el
@@ -193,8 +193,41 @@
 
 
 ;; LSP package
-(straight-use-package 'lsp-mode)
-(straight-use-package 'lsp-ui)
+(use-package lsp-mode
+  :straight t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (XXX-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
+;; optionally
+(use-package lsp-ui
+  :straight t
+  :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp
+  :straight t
+  :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+;(use-package lsp-ivy
+;  :commands lsp-ivy-workspace-symbol)
 
+(use-package lsp-treemacs
+  :straight t
+  :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+(use-package dap-mode
+  :straight t)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+    :straight t
+    :config
+    (which-key-mode))
 
